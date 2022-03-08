aad27ae84adb335fb94549720fddd33c748e35ea
unknown <epeng1994@gmail.com>
2022-03-08 15:34:17 -0500

    updated game function

--- index.js
+++ index.js
@@ -185,30 +185,28 @@ RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors
 */
 
 function game(user, computer){
-  if(user === 'rock'){
-    if(computer === 'scissor'){
-      return ('you win!')
-    }else if(computer === 'paper'){
-      return ('you lose!')
-    }else if(computer === 'rock'){
+  if(user === "rock"){
+    if(computer === "scissor"){
+      return ("you win!")
+    }else if(computer === "paper"){
+      return ("you lose!")
+    }else if(computer === "rock"){
       return ("it's a tie")
     }
-  }
-  if(user === 'paper'){
-    if(computer === 'rock'){
-      return ('you win!')
-    }else if(computer === 'scissor'){
-      return ('you lose!')
-    }else if(computer === 'paper'){
+  }else if(user === "paper"){
+    if(computer === "rock"){
+      return ("you win!")
+    }else if(computer === "scissor"){
+      return ("you lose!")
+    }else if(computer === "paper"){
       return ("it's a tie")
     }
-  }
-  if(user === 'scissor'){
-    if(computer === 'paper'){
-      return ('you win!')
-    }else if(computer === 'rock'){
-      return ('you lose!')
-    }else if(choice === 'scissor'){
+  }else if(user === "scissor"){
+    if(computer === "paper"){
+      return ("you win!")
+    }else if(computer === "rock"){
+      return ("you lose!")
+    }else if(choice === "scissor"){
       return ("it's a tie")
     }
   }
@@ -308,7 +306,7 @@ function grade(grade){
   }else if(grade >= 60 && grade <= 69){
     return ('you got a D')
   }else{
-    return ('you got a F')
+    return ('you got an F')
   }
 }
 
